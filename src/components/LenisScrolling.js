c05a4b2b4d78b425fdfd2f45c4b63ceb107c32a9
tuananhnvl <creatiflow.danny@gmail.com>
2024-04-10 01:54:41 +0700

    ad

--- src/components/LenisScrolling.js
+++ src/components/LenisScrolling.js
@@ -9,22 +9,22 @@ export default function LenisScrolling({ children }) {
   const lenisRef = React.useRef(null)
 
   React.useEffect(() => {
-    lenisRef.current.lenis = new Lenis({
+    lenisRef.current = new Lenis({
       duration: 2.5,
       lerp: 0.045
     })
 
 
     const raf = (time) => {
-      //lenisRef.current?.lenis?.raf(time * 1000);
+      lenisRef.current?.raf(time * 1000);
     };
     setTimeout(() => {
-      window.lenis = lenisRef.current.lenis
+      window.lenis = lenisRef.current
     }, 2000);
     gsap.ticker.add(raf)
     return () => {
       gsap.ticker.remove(raf)
-      lenisRef.current?.lenis?.destroy()
+     // lenisRef.current?.lenis?.destroy()
     }
   }, [lenisRef])
   
